f8b1246e9c1b5a72835846ad2714ade33d4fd3d7
AnderVillalobosGilling <113202207+AnderVillalobosGilling@users.noreply.github.com>
2022-10-05 11:38:47 -0500

    Update Calculadora.cpp
    acabo de entender como se hacen las car[etas de entrega    el bot no encontraba mi tarea porque no excistia la carpeta

--- Calculadora/Calculadora.cpp
+++ Calculadora/Calculadora.cpp
@@ -1,1 +1,49 @@
+#include <iostream>
 
+using namespace std;
+
+// declaracion de variables flotantes y operadores universales
+float a, b ;
+char  operador ;
+
+int main() {
+
+    cout << "hola este programa es una calculadora " << "te permite  hacer operaciones basicas entre dos numeros " << endl;
+// se le pide al usuario ingresar los valores con los que quiere trabajar y la operacion
+    cout << "primer numero : " << endl ;
+    cin >> a  ;
+    cout << " ingresa la operacion a realizar: " << endl ;
+    cin >> operador ;
+    cout << "segundo numero :  " << endl ;
+    cin >> b ;
+    // el usuario ya ingreso los valores ahora le damos instrucciones a la compu de como trabajar
+
+
+    if (operador == '+' ){
+        cout << a << operador << b << "=" << a+b << endl  ;
+    }
+    else if( operador  == '-'){
+
+        cout << a << operador << b << "=" << a-b << endl ;
+    }
+    else if (operador == '*') {
+        cout << a << operador << b << "=" << a*b << endl ;
+    }
+    else if (operador == '/') {
+        if (b == 0 ){ cout << "syntax error division no posible" << endl;
+        }
+        else {
+            cout << a << operador << b << "=" << a/b << endl;
+        }
+    }
+ // no sabemos como redondear floats a integros asi que lo pedimos que lo hagan ellos jsjsjsj
+    else if (operador == '%') {
+        cout << "favor de redondear sus numeros ya que no se hacerlo en el programa e ingresar nuevamente sin punto decimal " << endl ;
+        int d, e ;
+        cin >> d  ;
+        cin >> e  ;
+        cout << d << operador << e << "=" << d%e << endl ;
+    }
+
+    return 0;
+}
