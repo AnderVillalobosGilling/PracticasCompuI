6cd9f4ce3b2963de2088bf28a4eabb5733eccb78
AnderVillalobosGilling <113202207+AnderVillalobosGilling@users.noreply.github.com>
2022-11-16 01:04:53 -0600

    Update Punto.cpp

--- Puntos/Punto.cpp
+++ Puntos/Punto.cpp
@@ -11,11 +11,10 @@
 using namespace std;
 
 Punto2D::Punto2D() {
-
     a=0;
     b=0;
     c=0;
-    
+
 }
 
 void Punto2D::SetPosicion(float x, float y) {
@@ -28,3 +27,24 @@ void Punto2D::Trasladar(float temp1, float temp2) {
     b = b + temp2;
 }
 
+void Punto2D::RotarRespectoAlOrigen(float temp1) {
+    c = a;
+    a = c* cos(temp1*PI/180) - b* sin(temp1*PI/180);
+    b = c* sin(temp1*PI/180) - b* cos(temp1*PI/180);
+    //se genera la funcion para rotar "la mas complicada de hacer"
+}
+
+void Punto2D::Escalar(float temp1, float temp2) {
+    a = a*temp1;
+    b = b*temp2;
+
+}
+
+float Punto2D::GetX() {
+    return a;
+    //aqui las funciones para que nos de nuestro resultado
+}
+
+float Punto2D::GetY() {
+    return b;
+}
