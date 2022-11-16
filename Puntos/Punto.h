986b5fa7d368800848c5d409a35c6a8abb1cada5
AnderVillalobosGilling <113202207+AnderVillalobosGilling@users.noreply.github.com>
2022-11-16 00:40:56 -0600

    Update Punto.h

--- Puntos/Punto.h
+++ Puntos/Punto.h
@@ -1,1 +1,31 @@
+//
+// Created by ander on 11/10/2022.
+//
+
+#ifndef PUNTOS_PUNTO2D_H
+#define PUNTOS_PUNTO2D_H
+#include <vector>
+
+using namespace std;
+
+class Punto2D {
+//creamos nuestro ambiente publico para los metodos
+public:
+    Punto2D();
+    void SetPosicion(float x, float y);
+    void Trasladar(float temp1, float temp2);
+    void RotarRespectoAlOrigen(float temp1);
+    void Escalar(float temp1, float temp2);
+    float GetX();
+    float GetY();
+
+private:
+
+    float a , b , c;
+    
+
+};
+
+
+#endif //PUNTOS_PUNTO2D_H
 
