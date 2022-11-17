d37f2df41f3d35e005a4e44b176956012fc3d7ac
AnderVillalobosGilling <113202207+AnderVillalobosGilling@users.noreply.github.com>
2022-11-17 15:49:54 -0600

    Update Punto.cpp
    cambiamos todo a ver si si jala

--- Puntos/Punto.cpp
+++ Puntos/Punto.cpp
@@ -1,23 +1,20 @@
 //
 // Created by ander on 11/10/2022.
 //
-//metemos varios otros includes que necesita el codigo
-#include "Punto2D.h"
 #include <iostream>
 #include <math.h>
-// definimos el valor de PI
 #define PI 3.14159265
+#include "Punto.h"
 
 using namespace std;
 
 Punto2D::Punto2D() {
-    a=0;
-    b=0;
-    c=0;
-
+    a = 0;
+    b = 0;
+    c = 0;
 }
 
-void Punto2D::SetPosicion(float x, float y) {
+void Punto2D::SetPosicion(float x,float y){
     a = x;
     b = y;
 }
@@ -27,22 +24,19 @@ void Punto2D::Trasladar(float temp1, float temp2) {
     b = b + temp2;
 }
 
-void Punto2D::RotarRespectoAlOrigen(float temp1) {
+void Punto2D::RotarRespectoAlOrigen(float temp1){
     c = a;
-    a = c* cos(temp1*PI/180) - b* sin(temp1*PI/180);
-    b = c* sin(temp1*PI/180) - b* cos(temp1*PI/180);
-    //se genera la funcion para rotar "la mas complicada de hacer"
+    a = c*cos(temp1*PI/180) - b*sin(temp1*PI/180);
+    b = c*sin(temp1*PI/180) + b*cos(temp1*PI/180);
 }
 
 void Punto2D::Escalar(float temp1, float temp2) {
     a = a*temp1;
     b = b*temp2;
-
 }
 
 float Punto2D::GetX() {
     return a;
-    //aqui las funciones para que nos de nuestro resultado
 }
 
 float Punto2D::GetY() {
