12caa2ea3e12db29876cb39d6caa8df33eadec87
AnderVillalobosGilling <113202207+AnderVillalobosGilling@users.noreply.github.com>
2022-11-23 11:17:08 -0600

    Create Grupo.cpp

---
+++ CargaElectrica/Grupo.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Ander on 15/11/2022.
+//
+
+#include "Grupo.h"
+#include <string>
+#include <array>
+
+using namespace std;
+
+std::string Grupo::getNombre() {
+    return NombreGrupo;
+
+}
+float Grupo::getPk() {
+    return pKGrupo;
+}
+
+int Grupo::getCargagrupo() {
+    return CargaGrupo;
+}
